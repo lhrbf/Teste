8090ee0bc4421f86fb896c5cc401b51a04d515c3
lhrbf <l.henriquerbf@gmail.com>
2025-01-28 16:09:31 -0300

    1

--- plataforma/resources/views/cadastro.blade.php
+++ plataforma/resources/views/cadastro.blade.php
@@ -14,56 +14,66 @@
     </div>
     
     <div class="container">
-        <div class="card text-center my-5 border-primary-subtle">
-            <div class="card-header bg-body-tertiary fw-medium border-primary-subtle">
+        <div class="card justify-content-center d-flex my-5 border-black">
+            <div class="card-header bg-dark text-white fw-medium border-black">
               CADASTRO
             </div>
             <div class="card-body">
                 <form method="POST" action="{{ route('register') }}">
-                    <div class="row justify-content-center d-flex mb-3">
-                        <label for="name" class="col-12 w-75 justify-content-start d-flex form-label">Nome completo</label>
-                        <input type="text" class="border-primary-subtle col-12 w-75 form-control" id="name" placeholder="Digite seu nome completo" required>
+                    <div class="row justify-content-center d-flex">
+                        <div class="col-12 col-lg-5 col-md-4 mb-3">
+                            <label for="name" class="form-label mb-1">Nome completo</label>
+                            <input type="text" class="form-control border-dark" id="name" placeholder="Digite seu nome completo" required>
+                        </div>
+                            <div class="col-12 col-lg-5 col-md-4 mb-3">
+                                <label for="email" class="form-label mb-1">Email</label>
+                                <input type="email" class="form-control border-dark" id="email" placeholder="Digite o Email" required>
+                            </div>
                     </div>
-                    <div class="row justify-content-center d-flex mb-3">
-                        <label for="email" class="col-12 w-75 justify-content-start d-flex form-label">Email</label>
-                        <input type="email" class="border-primary-subtle col-12 w-75 form-control" id="email" placeholder="Digite o Email" required>
+                    <div class="row justify-content-center d-flex">
+                        <div class="col-12 col-lg-5 col-md-4 mb-3">
+                            <label for="cpf" class="form-label mb-1">CPF</label>
+                            <input type="text" class="form-control border-dark" id="cpf" placeholder="Digite o CPF" required>
+                        </div>
+                            <div class="col-12 col-lg-5 col-md-4 mb-3">
+                                <label for="estado" class="form-label mb-1">Selecione um estado</label>
+                                <select id="estado" class="form-control border-dark" name="estado" required>
+                                    <option value="">Selecione um estado</option>
+                                </select>
+                            </div>
                     </div>
-                    <div class="row justify-content-center d-flex mb-3">
-                        <label for="cpf" class="col-12 w-75 justify-content-start d-flex form-label">CPF</label>
-                        <input type="text" class="border-primary-subtle col-12 w-75 form-control" id="cpf" placeholder="Digite o CPF" required>
+                    <div class="row justify-content-center d-flex">
+                        <div class="col-12 col-lg-5 col-md-4 mb-3">
+                            <label for="cep" class="form-label mb-1">CEP</label>
+                            <input type="text" id="cep" class="form-control border-dark" name="cep" placeholder="Digite o CEP" required>
+                        </div>
+                            <div class="col-12 col-lg-5 col-md-4 mb-3">
+                                <label for="rua" class="form-label mb-1">Rua</label>
+                                <input type="text" id="rua" class="form-control border-dark" name="rua" placeholder="Digite o nome da rua" required>
+                            </div>
                     </div>
-                    <div class="row justify-content-center d-flex" >
-                        <label for="estado" class="w-75 justify-content-start d-flex">Selecione um estado</label>
-                        <select id="estado" class="border-primary-subtle col-12 w-75 form-control" name="estado" required>
-                            <option value="">Selecione um estado</option>
-                        </select>
+                    <div class="row justify-content-center d-flex">
+                        <div class="col-12 col-lg-5 col-md-4 mb-3">
+                            <label for="numero" class="form-label mb-1">Número</label>
+                            <input type="number" id="numero" class="form-control border-dark" placeholder="Número da residência" required>
+                        </div>
+                            <div class="col-12 col-lg-5 col-md-4 mb-3">
+                                <label for="inputNumber" class="form-label mb-1">Número de telefone</label>
+                                <input type="tel" id="inputNumber" class="form-control border-dark" placeholder="Digite o número de telefone" required>
+                            </div>
                     </div>
-                    <div class="row justify-content-center d-flex mt-3">
-                        <label for="cep" class="w-75 justify-content-start d-flex">CEP</label>
-                        <input type="text" id="cep" class="border-primary-subtle col-12 w-75 form-control" name="cep" placeholder="Digite o CEP" required>
+                    <div class="row justify-content-center d-flex">
+                        <div class="col-12 col-lg-5 col-md-4 mb-3">
+                            <label for="inputPassword" class="form-label mb-1">Senha</label>
+                            <input type="password" id="inputPassword" name="password" class="form-control border-dark" placeholder="Digite uma senha" required>
+                        </div>
+                            <div class="col-12 col-lg-5 col-md-4 mb-3">
+                                <label for="inputPasswordConfirmation" class="form-label mb-1">Confirme a senha</label>
+                                <input type="password" id="inputPasswordConfirmation" name="passwordConfirmation" class="form-control border-dark" placeholder="Confirme a senha" required>
+                            </div>
                     </div>
-                    <div class="row justify-content-center d-flex mt-3">
-                        <label for="rua" class="w-75 justify-content-start d-flex">Rua</label>
-                        <input type="text" id="rua" class="border-primary-subtle col-12 w-75 form-control" name="rua" placeholder="Digite o nome da rua" required>
-                    </div>
-                    <div class="row justify-content-center d-flex form-group mt-3">
-                        <label for="numero" class="w-75 justify-content-start d-flex">Número</label>
-                        <input type="number" id="numero" class="border-primary-subtle col-12 w-75 form-control" placeholder="Número da residência" required>
-                    </div>
-                    <div class="row justify-content-center d-flex mt-3">
-                        <label for="inputNumber" class="col-12 w-75 justify-content-start d-flex form-label">Número de telefone</label>
-                        <input type="tel" id="inputNumber" class="border-primary-subtle col-12 w-75 form-control" placeholder="Digite o número de telefone" required>
-                    </div>
-                    <div class="row justify-content-center d-flex mt-3">
-                        <label for="inputPassword" class="col-12 w-75 justify-content-start d-flex form-label">Senha</label>
-                        <input type="password" id="inputPassword" name="password" class="border-primary-subtle col-12 w-75 form-control" placeholder="Digite uma Senha" aria-describedby="passwordHelpBlock" required>
-                    </div>
-                    <div class="row justify-content-center d-flex mt-3"> 
-                        <label for="inputPassword" class="col-12 w-75 justify-content-start d-flex form-label">Confirme a Senha</label>
-                        <input type="password" id="inputPasswordConfirmation" class="border-primary-subtle col-12 w-75 form-control" name="passwordConfirmation" placeholder="Confirme a senha" required>
-                    </div>
-                    <div class="mt-3">
-                        <button type="submit" class="btn btn-primary w-auto px-4 my-2 fs-5">Cadastrar</button>
+                    <div class="mt-3 mb-2 justify-content-center d-flex">
+                        <button type="submit" class="btn btn-primary w-auto px-4 fs-5">Cadastrar</button>
                     </div>
                 </form>
             </div>
--- plataforma/resources/views/login.blade.php
+++ plataforma/resources/views/login.blade.php
@@ -9,38 +9,43 @@
 </head>
     <body>
         <div class="container">
-            <div class="card text-center border-primary-subtle mt-5">
-                <div class="card-header border-primary-subtle bg-body-tertiary fw-medium">
+            <div class="card border-dark mt-5">
+                <div class="card-header border-dark bg-dark text-white fw-medium">
                     LOGIN
                 </div>
-                <div class="card-body">
+                <div class="card-body bg-body-tertiary">
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
                         
-                        <div class="row justify-content-center d-flex mb-3">
-                            <label for="email" class="col-12 form-label justify-content-start d-flex w-75">Email</label>
-                            <input type="email" name="email" class="border-primary-subtle col-12 w-75 form-control" id="email" placeholder="Digite o Email" autocomplete="email" required>
-                            @error('email')
-                            <span class="text-danger">{{ $message }}</span>
-                            @enderror
+                        <div class="row justify-content-center d-flex">
+                            <div class="col-12 col-md-10 col-lg-7 mb-3">
+                                <label for="email" class="form-label mb-1">Email</label>
+                                <input type="email" name="email" class="form-control border-dark" id="email" placeholder="Digite o Email" autocomplete="email" required>
+                                @error('email')
+                                <span class="text-danger">{{ $message }}</span>
+                                @enderror
+                            </div>
+        
+                            <div class="col-12 col-md-10 col-lg-7 mb-3">
+                                <label for="inputPassword" class="form-label mb-1">Senha</label>
+                                <input type="password" name="password" id="inputPassword" class="form-control border-dark" autocomplete="current-password" required>
+                            </div>
+        
+                            <div class="col-12 col-md-5 col-lg-6 mb-3 text-center">
+                                <button type="submit" class="btn btn-primary w-75 px-4 mt-2 fs-5">Entrar</button>
+                            </div>
+                        </div>
+        
+                    </form>
+                    <div class="row justify-content-center d-flex mt-3">
+                        <div class="col-12 justify-content-center d-flex text-center">
+                            <a href="{{ route('cadastro')}}" class="btn btn-outline-primary">Não tem login? Clique aqui</a>
                         </div>
                         
-                        <div class="row justify-content-center d-flex my-3">
-                            <label for="inputPassword" class="col-12 form-label justify-content-start d-flex w-75">Password</label>
-                            <input type="password" name="password" id="inputPassword" class="border-primary-subtle col-12 w-75 form-control" autocomplete="current-password" required>
-                        </div>
-
-                        <div>
-                            <button type="submit" class="btn btn-primary w-auto px-4 my-2 fs-5">Entrar</button>
-                        </div>
-                    </form>
-            
-                    <div class="card-footer border-primary-subtle bg-white">
-                        <a href="{{ route('cadastro')}}" class="mt-1 mb-0 btn btn-outline-primary">Não tem login? Clique aqui</a>
                     </div>
                 </div>
             </div>
-        </div>    
+        </div>
                 
         <script src="https://cdn.jsdelivr.net/npm/inputmask@5.0.6/dist/inputmask.min.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
